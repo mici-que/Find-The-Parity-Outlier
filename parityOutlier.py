f304af621e39abfeefcc23e4598652ad330bf8cf
mici-que <Maria.Kohler@ibm.com>
2022-04-28 12:16:57 +0200

    # 1 GREEN

--- parityOutlier.py
+++ parityOutlier.py
@@ -1,2 +1,11 @@
 def find_outlier (integers=None):
+    if not ValidInput(integers): return False
     pass
+
+
+def ValidInput(a=None):
+    if ('a' in vars() # param defined
+    and isinstance(a,list) # param is list
+    and len(a)>=3 # list has at least 3 entries
+    and (all(isinstance(n,int) for n in a))): # list entries are ints
+       return True
