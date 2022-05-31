d85b0d5b81e31425d01b04f14b4a0ca76499d22b
mici-que <Maria.Kohler@ibm.com>
2022-05-31 16:02:37 +0200

    lint once more

--- parityOutlier.py
+++ parityOutlier.py
@@ -1,15 +1,15 @@
 def     find_outlier( integers=None ):
 
-    if findoutlier(integers):
+    if findoutlier (integers) :
         if not ValidInput(integers):
             return False
         remaindersList = [x % 2 for x in integers]
         if sum(remaindersList) == 1:
             return integers[remaindersList.index(1)]
         else:
-            return integers[remaindersList.index(0)]
+            return  integers[remaindersList.index(0)]
 
-def ValidInput(a=None ):
+def ValidInput ( a=None ):
     if (
         "a" in vars()  # param defined
         and isinstance(a, list)  # param is list
