7f46d47f315cccd5157011110c00006992eab4c7
mici-que <Maria.Kohler@ibm.com>
2022-06-01 12:35:41 +0200

    removeFUnc

--- parityOutlier.py
+++ parityOutlier.py
@@ -1,15 +1,14 @@
-def     find_outlier( integers=None ):
+def find_outlier(integers=None):
+    if not ValidInput(integers):
+        return False
+    remaindersList = [x % 2 for x in integers]
+    if sum(remaindersList) == 1:
+        return integers[remaindersList.index(1)]
+    else:
+        return integers[remaindersList.index(0)]
 
-    if findoutlier (integers) :
-        if not ValidInput(integers):
-            return False
-        remaindersList = [x % 2 for x in integers]
-        if sum(remaindersList) == 1:
-            return integers[remaindersList.index(1)]
-        else:
-            return  integers[remaindersList.index(0)]
 
-def ValidInput ( a=None ):
+def ValidInput(a=None):
     if (
         "a" in vars()  # param defined
         and isinstance(a, list)  # param is list
@@ -17,5 +16,3 @@ def ValidInput ( a=None ):
         and (all(isinstance(n, int) for n in a))
     ):  # list entries are ints
         return True
-def findoutlier(integers=None):
-    return True
