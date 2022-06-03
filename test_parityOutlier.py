f8fe7fda3730656808f78249250cb94df2ef85ee
mici-que <Maria.Kohler@ibm.com>
2022-06-03 13:54:56 +0200

    c

--- test_parityOutlier.py
+++ test_parityOutlier.py
@@ -4,14 +4,14 @@ import parityOutlier
 # 1
 def testparityOutlier_NoInput() -> None:
     """If no parameters passed, return False"""
-    assert parityOutlier.find_outlier() is False, "no input, should return false"
+    assert parityOutlier.findOutlier() is False, "no input, should return false"
 
 
 def testparityOutlier_WrongInputType() -> None:
     """paramter isn't a list, return False"""
     parameter = "string"
     assert (
-        parityOutlier.find_outlier(parameter) is False
+        parityOutlier.findOutlier(parameter) is False
     ), "input isn't list, should return false"
 
 
@@ -19,7 +19,7 @@ def testparityOutlier_WrongListEntryType() -> None:
     """list entries are not ints, return False"""
     parameter = ["1", "2", "3", "4"]
     assert (
-        parityOutlier.find_outlier(parameter) is False
+        parityOutlier.findOutlier(parameter) is False
     ), "list entries are not ints, should return false"
 
 
@@ -27,7 +27,7 @@ def testparityOutlier_ShortList() -> None:
     """list should have minimum 3 entries, return False"""
     parameter = [1, 2]
     assert (
-        parityOutlier.find_outlier(parameter) is False
+        parityOutlier.findOutlier(parameter) is False
     ), "list is too short, should return false"
 
 
@@ -35,10 +35,10 @@ def testparityOutlier_ShortList() -> None:
 def testparityOutlier_List0() -> None:
     """[1,1,2] -> 2"""
     parameter = [1, 1, 2]
-    assert parityOutlier.find_outlier(parameter) == 2, "[1,1,2] -> 2"
+    assert parityOutlier.findOutlier(parameter) == 2, "[1,1,2] -> 2"
 
 
 def testparityOutlier_List1() -> None:
     """[2, 4, 6, 8, 10, 3] -> 3"""
     parameter = [2, 4, 6, 8, 10, 3]
-    assert parityOutlier.find_outlier(parameter) == 3, "[2, 4, 6, 8, 10, 3] -> 3"
+    assert parityOutlier.findOutlier(parameter) == 3, "[2, 4, 6, 8, 10, 3] -> 3"
