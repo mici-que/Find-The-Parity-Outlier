31d37551ef0be5bfc5c83a1e56a3be1995394bd2
mici-que <Maria.Kohler@ibm.com>
2022-06-13 13:18:32 +0200

    uncovered funk

--- parityOutlier.py
+++ parityOutlier.py
@@ -1,22 +1,23 @@
 def findOutlier(integers=None):
-    if not validInput(integers):
-        return False
-    remaindersList = createRemainderslist(integers)
-    if sum(remaindersList) == 1:
-        return integers[remaindersList.index(1)]
-    else:
-        return integers[remaindersList.index(0)]
+    if validInput(integers):
+        remaindersList = [x % 2 for x in integers]
+        remaindersSum = sum(remaindersList)
+        # remaindersSum = 1, return odd position integers(index of 1 in remaindersList)
+        # remaindersSum > 1, return even position integers(index of 0 in remaindersList)
+        #
+        return integers[remaindersList.index(not bool(remaindersSum - 1))]
+    return False
 
 
 def validInput(a=None):
-    if (
+    return (
         "a" in vars()  # param defined
         and isinstance(a, list)  # param is list
         and len(a) >= 3  # list has at least 3 entries
-        and (all(isinstance(n, int) for n in a))
-    ):  # list entries are ints
-        return True
+        and (all(isinstance(n, int) for n in a))  # list entries are ints
+    )
 
 
-def createRemainderslist(integers):
-    return [x % 2 for x in integers]
+def function_not_covered(a):
+    if a == None:
+        return "none"
